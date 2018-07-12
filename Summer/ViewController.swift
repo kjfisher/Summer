b7edc7d108584cd53f889949ace73c64aeda6fc9
Kristen L. Fisher <kristen.fisher@eg-mba-kfisher.d214.org>
2018-07-12 09:46:33 -0500

    Initial Commit

---
+++ Summer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Summer
+//
+//  Created by Kristen L. Fisher on 7/12/18.
+//  Copyright © 2018 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
